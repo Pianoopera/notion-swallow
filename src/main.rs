6f1e0d54451ac7b432ddc51183aaab71916088d5
Pianoopera <azing3469@gmail.com>
2024-08-16 08:52:39 +0900

    cargo crul

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    // args を取得する
+    let args: Vec<String> = std::env::args().collect();
+    // https://api.notion.com/v1/${args[1]} を出力する
+    println!("https://api.notion.com/v1/{}", args[1]);
 }
--- tests/cli.rs
+++ tests/cli.rs
@@ -3,11 +3,14 @@ use assert_cmd::Command;
 #[test]
 fn runs() {
     let mut cmd = Command::cargo_bin("rf-notion").unwrap();
+    cmd.arg("databases");
     cmd.assert().success();
 }
 
 #[test]
 fn runs_output() {
     let mut cmd = Command::cargo_bin("rf-notion").unwrap();
-    cmd.assert().success().stdout("Hello, world!\n");
+    // notion api の curl コマンドを作成する
+    cmd.arg("hoge");
+    cmd.assert().success().stdout("https://api.notion.com/v1/hoge\n");
 }
