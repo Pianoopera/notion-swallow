a0f318421b655e68a11e05659912125e12f95b7d
Pianoopera <azing3469@gmail.com>
2024-08-26 08:35:48 +0900

    v0.2.3

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,29 @@
-mod method;
 mod args;
 mod cmds;
+mod method;
 mod subcommand;
 
 use clap::App;
 use cmds::{
-    append_blocks_cmd, blocks_cmd, children_blocks_cmd, databases_cmd, i_cmd::ICommand, pages_cmd, property_pages_cmd, query_databases_cmd, search_cmd
+    append_blocks_cmd, blocks_cmd, children_blocks_cmd, databases_cmd, i_cmd::ICommand, pages_cmd,
+    property_pages_cmd, query_databases_cmd, search_cmd,
 };
 
 fn main() {
-    let app = App::new("rf-notion")
-        .version("0.1.0")
+    let app = App::new("notion-swallow")
+        .version("0.2.3")
         .author("teto <https://github.com/Pianoopera>")
         .about("Output Notion API URLs")
-        .subcommands(
-            vec![
-                databases_cmd::databases_subcommand(),
-                query_databases_cmd::query_databases_cmd(),
-                pages_cmd::pages_subcommand(),
-                property_pages_cmd::property_pages_subcommand(),
-                append_blocks_cmd::append_blocks_subcommand(),
-                blocks_cmd::blocks_subcommand(),
-                children_blocks_cmd::children_blocks_subcommand(),
-                search_cmd::search_subcommand()
-            ]
-        );
+        .subcommands(vec![
+            databases_cmd::databases_subcommand(),
+            query_databases_cmd::query_databases_cmd(),
+            pages_cmd::pages_subcommand(),
+            property_pages_cmd::property_pages_subcommand(),
+            append_blocks_cmd::append_blocks_subcommand(),
+            blocks_cmd::blocks_subcommand(),
+            children_blocks_cmd::children_blocks_subcommand(),
+            search_cmd::search_subcommand(),
+        ]);
     let arg_matches = app.get_matches();
 
     let notion_api_key = dotenv::var("NOTION_API_KEY").unwrap_or("default".to_string());
@@ -34,25 +33,25 @@ fn main() {
     match subcommand {
         subcommand::NotionSubCommand::Databases(databases) => {
             databases.print_curl(notion_api_key, notion_version);
-        },
+        }
         subcommand::NotionSubCommand::QueryDatabases(query_databases) => {
             query_databases.print_curl(notion_api_key, notion_version);
-        },
+        }
         subcommand::NotionSubCommand::Pages(pages) => {
             pages.print_curl(notion_api_key, notion_version);
-        },
+        }
         subcommand::NotionSubCommand::PropertyPages(property_pages) => {
             property_pages.print_curl(notion_api_key, notion_version);
-        },
+        }
         subcommand::NotionSubCommand::BlocksAppend(append_blocks) => {
             append_blocks.print_curl(notion_api_key, notion_version);
-        },
+        }
         subcommand::NotionSubCommand::Blocks(blocks) => {
             blocks.print_curl(notion_api_key, notion_version);
-        },
+        }
         subcommand::NotionSubCommand::ChildrenBlocks(children_blocks) => {
             children_blocks.print_curl(notion_api_key, notion_version);
-        },
+        }
         subcommand::NotionSubCommand::Search(search) => {
             search.print_curl(notion_api_key, notion_version);
         }
--- tests/cli.rs
+++ tests/cli.rs
@@ -3,19 +3,18 @@ use std::fs;
 
 type TestResult = Result<(), Box<dyn std::error::Error>>;
 
-
 #[test]
 fn runs() {
-    let mut cmd = Command::cargo_bin("rf-notion").unwrap();
+    let mut cmd = Command::cargo_bin("notion-swallow").unwrap();
     cmd.arg("databases");
     cmd.assert().success();
 }
 
 fn runs_output(args: &[&str], expected_file: &str) -> TestResult {
     let expected = fs::read_to_string(expected_file)?;
-    
+
     // notion api の curl コマンドを作成する
-    Command::cargo_bin("rf-notion")
+    Command::cargo_bin("notion-swallow")
         .unwrap()
         .args(args)
         .assert()
@@ -31,148 +30,182 @@ fn databases1() -> TestResult {
 
 #[test]
 fn databases2() -> TestResult {
-    runs_output(&["databases", "--id", "12345678-1234-1234-1234-123456789012"], "tests/expected/databases2.txt")
+    runs_output(
+        &["databases", "--id", "12345678-1234-1234-1234-123456789012"],
+        "tests/expected/databases2.txt",
+    )
 }
 
 #[test]
 fn databases_post1() -> TestResult {
-    runs_output(&[
-        "databases",
-        "-x",
-        "POST",
-        "--file",
-        "tests/fixture/post/request1.json",
-    ], "tests/expected/databases_post1.txt")
+    runs_output(
+        &[
+            "databases",
+            "-x",
+            "POST",
+            "--file",
+            "tests/fixture/post/request1.json",
+        ],
+        "tests/expected/databases_post1.txt",
+    )
 }
 
 #[test]
 fn query_databases_post() -> TestResult {
-    runs_output(&[
-        "query_databases",
-        "--id",
-        "897e5a76ae524b489fdfe71f5945d1af",
-        "--file",
-        "tests/fixture/post/request2.json",
-    ], "tests/expected/query_databases_post2.txt")
+    runs_output(
+        &[
+            "query_databases",
+            "--id",
+            "897e5a76ae524b489fdfe71f5945d1af",
+            "--file",
+            "tests/fixture/post/request2.json",
+        ],
+        "tests/expected/query_databases_post2.txt",
+    )
 }
 
 #[test]
 fn databases_patch1() -> TestResult {
-    runs_output(&[
-        "databases",
-        "-x",
-        "PATCH",
-        "-i",
-        "668d797c-76fa-4934-9b05-ad288df2d136",
-        "-f",
-        "tests/fixture/patch/request1.json",
-    ], "tests/expected/databases_patch1.txt")
+    runs_output(
+        &[
+            "databases",
+            "-x",
+            "PATCH",
+            "-i",
+            "668d797c-76fa-4934-9b05-ad288df2d136",
+            "-f",
+            "tests/fixture/patch/request1.json",
+        ],
+        "tests/expected/databases_patch1.txt",
+    )
 }
 
 #[test]
 fn pages_create() -> TestResult {
-    runs_output(&[
-        "pages",
-        "-x",
-        "POST",
-        "-f",
-        "tests/fixture/post/create_request1.json",
-    ], "tests/expected/pages_create1.txt")
+    runs_output(
+        &[
+            "pages",
+            "-x",
+            "POST",
+            "-f",
+            "tests/fixture/post/create_request1.json",
+        ],
+        "tests/expected/pages_create1.txt",
+    )
 }
 
 #[test]
 fn pages_retrieve() -> TestResult {
-    runs_output(&[
-        "pages",
-        "-i",
-        "12345678-1234-1234-1234-123456789012",
-    ], "tests/expected/pages_retrieve1.txt")
+    runs_output(
+        &["pages", "-i", "12345678-1234-1234-1234-123456789012"],
+        "tests/expected/pages_retrieve1.txt",
+    )
 }
 
 #[test]
 fn pages_property_item() -> TestResult {
-    runs_output(&[
-        "property_pages",
-        "-i",
-        "12345678-1234-1234-1234-123456789012",
-        "-p",
-        "aBcd123",
-    ], "tests/expected/property_pages_retrieve1.txt")
+    runs_output(
+        &[
+            "property_pages",
+            "-i",
+            "12345678-1234-1234-1234-123456789012",
+            "-p",
+            "aBcd123",
+        ],
+        "tests/expected/property_pages_retrieve1.txt",
+    )
 }
 
 #[test]
 fn pages_updated() -> TestResult {
-    runs_output(&[
-        "pages",
-        "-x",
-        "PATCH",
-        "-i",
-        "12345678-1234-1234-1234-123456789012",
-        "-f",
-        "tests/fixture/patch/pages_patch_request1.json",
-    ], "tests/expected/pages_patch1.txt")
+    runs_output(
+        &[
+            "pages",
+            "-x",
+            "PATCH",
+            "-i",
+            "12345678-1234-1234-1234-123456789012",
+            "-f",
+            "tests/fixture/patch/pages_patch_request1.json",
+        ],
+        "tests/expected/pages_patch1.txt",
+    )
 }
 
 #[test]
 fn append_blocks() -> TestResult {
-    runs_output(&[
-        "append_blocks",
-        "-x",
-        "PATCH",
-        "-i",
-        "12345678-1234-1234-1234-123456789012",
-        "-f",
-        "tests/fixture/patch/append_blocks_request1.json",
-    ], "tests/expected/append_blocks1.txt")
+    runs_output(
+        &[
+            "append_blocks",
+            "-x",
+            "PATCH",
+            "-i",
+            "12345678-1234-1234-1234-123456789012",
+            "-f",
+            "tests/fixture/patch/append_blocks_request1.json",
+        ],
+        "tests/expected/append_blocks1.txt",
+    )
 }
 
 #[test]
 fn update_block() -> TestResult {
-    runs_output(&[
-        "blocks",
-        "-x",
-        "PATCH",
-        "-i",
-        "12345678-1234-1234-1234-123456789012",
-        "-f",
-        "tests/fixture/patch/blocks_update_request1.json",
-    ], "tests/expected/blocks_update1.txt")
+    runs_output(
+        &[
+            "blocks",
+            "-x",
+            "PATCH",
+            "-i",
+            "12345678-1234-1234-1234-123456789012",
+            "-f",
+            "tests/fixture/patch/blocks_update_request1.json",
+        ],
+        "tests/expected/blocks_update1.txt",
+    )
 }
 
 #[test]
 fn delete_block() -> TestResult {
-    runs_output(&[
-        "blocks",
-        "-x",
-        "DELETE",
-        "-i",
-        "12345678-1234-1234-1234-123456789012",
-    ], "tests/expected/bloks_delete1.txt")
+    runs_output(
+        &[
+            "blocks",
+            "-x",
+            "DELETE",
+            "-i",
+            "12345678-1234-1234-1234-123456789012",
+        ],
+        "tests/expected/bloks_delete1.txt",
+    )
 }
 
 #[test]
 fn ret_blocks() -> TestResult {
-    runs_output(&[
-        "blocks",
-        "-i",
-        "12345678-1234-1234-1234-123456789012"
-    ], "tests/expected/blocks_ret1.txt")
+    runs_output(
+        &["blocks", "-i", "12345678-1234-1234-1234-123456789012"],
+        "tests/expected/blocks_ret1.txt",
+    )
 }
 
 #[test]
 fn block_children() -> TestResult {
-    runs_output(&[
-        "children_blocks",
-        "-i",
-        "12345678-1234-1234-1234-123456789012"
-    ], "tests/expected/child_blocks.txt")
+    runs_output(
+        &[
+            "children_blocks",
+            "-i",
+            "12345678-1234-1234-1234-123456789012",
+        ],
+        "tests/expected/child_blocks.txt",
+    )
 }
 
 #[test]
 fn search_by_title() -> TestResult {
-    runs_output(&[
-        "search",
-        "-f",
-        "tests/fixture/post/search_by_title_request1.json",
-    ], "tests/expected/search_by_title.txt")
+    runs_output(
+        &[
+            "search",
+            "-f",
+            "tests/fixture/post/search_by_title_request1.json",
+        ],
+        "tests/expected/search_by_title.txt",
+    )
 }
